28a40135bf50363610b7c42a45de9933c4143b4d
jynito <31843779+jynito@users.noreply.github.com>
2018-12-03 09:58:49 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ var config = {
   // and branch to get files from.
   github: {
     username: 'jynito',
-    repo: 'jynito.github.io-mycms',
+    repo: 'mycms',
     branch: 'gh-pages',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
